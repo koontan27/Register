d7d9b2e82615af54f86ca48f6899c6aa978fb673
koontan27 <koontan27@gmail.com>
2018-09-13 00:46:44 +0700

    Finish

---
+++ Register/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.ConfirmPassword = new System.Windows.Forms.TextBox();
+            this.Reset = new System.Windows.Forms.PictureBox();
+            this.ok = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Reset)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ok)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Username
+            // 
+            this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Username.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Username.Location = new System.Drawing.Point(86, 150);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(158, 27);
+            this.Username.TabIndex = 0;
+            this.Username.TabStop = false;
+            this.Username.Text = "Username";
+            this.Username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Username.Click += new System.EventHandler(this.Username_Click);
+            // 
+            // Password
+            // 
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Password.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Password.Location = new System.Drawing.Point(86, 212);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(158, 27);
+            this.Password.TabIndex = 1;
+            this.Password.TabStop = false;
+            this.Password.Text = "Password";
+            this.Password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Password.Click += new System.EventHandler(this.Password_Click);
+            // 
+            // ConfirmPassword
+            // 
+            this.ConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.ConfirmPassword.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.ConfirmPassword.Location = new System.Drawing.Point(86, 271);
+            this.ConfirmPassword.Name = "ConfirmPassword";
+            this.ConfirmPassword.Size = new System.Drawing.Size(158, 27);
+            this.ConfirmPassword.TabIndex = 2;
+            this.ConfirmPassword.TabStop = false;
+            this.ConfirmPassword.Text = "Confirm Password";
+            this.ConfirmPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ConfirmPassword.Click += new System.EventHandler(this.ConfirmPassword_Click);
+            this.ConfirmPassword.TextChanged += new System.EventHandler(this.ConfirmPassword_TextChanged);
+            // 
+            // Reset
+            // 
+            this.Reset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Reset.Image = ((System.Drawing.Image)(resources.GetObject("Reset.Image")));
+            this.Reset.Location = new System.Drawing.Point(48, 314);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(85, 42);
+            this.Reset.TabIndex = 3;
+            this.Reset.TabStop = false;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // ok
+            // 
+            this.ok.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ok.Image = ((System.Drawing.Image)(resources.GetObject("ok.Image")));
+            this.ok.Location = new System.Drawing.Point(158, 314);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(86, 42);
+            this.ok.TabIndex = 4;
+            this.ok.TabStop = false;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(113, 167);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(10, 10);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(296, 389);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.ConfirmPassword);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Reset)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ok)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.TextBox ConfirmPassword;
+        private System.Windows.Forms.PictureBox Reset;
+        private System.Windows.Forms.PictureBox ok;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Register/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+
+namespace Register
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MongoClient client = new MongoClient("mongodb://admin:a123456@ds141902.mlab.com:41902/ox");
+                MongoServer server = client.GetServer();
+                MongoDatabase database = server.GetDatabase("ox");
+                MongoCollection mongoCollection = database.GetCollection<User>("User");
+                User user = new User();
+                BindingList<User> doclist = new BindingList<User>();
+                var getCollection = database.GetCollection<User>("User");
+                var insertUser = getCollection.AsQueryable().Where(pd => pd.Username == Username.Text);
+
+                foreach (var p in insertUser)
+                {
+                    doclist.Add(p);
+                    Application.DoEvents();
+                }
+                dataGridView1.DataSource = doclist;
+                if (dataGridView1.Rows.Count == 0)
+                {
+                    if (!string.IsNullOrEmpty(Username.Text.Trim()) && !Username.Text.Equals("Username"))
+                    {
+                        user.Username = Username.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("กรุณากรอก Username");
+                    }
+                    if (!string.IsNullOrEmpty(Password.Text.Trim()) && !Password.Text.Equals("Password"))
+                    {
+                        user.Password = Password.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("กรุณากรอก Password");
+                    }
+                    if (!string.IsNullOrEmpty(ConfirmPassword.Text.Trim()) && !ConfirmPassword.Text.Equals("Confirm Password"))
+                    {
+                        user.Password = ConfirmPassword.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("กรุณากรอก Comfirm Password");
+                    }
+
+                    user.Name = Username.Text;
+                    user.Avatar = null;
+                    user.Win = 0;
+                    user.Draw = 0;
+                    user.Lose = 0;
+
+                    if (!string.IsNullOrEmpty(Username.Text.Trim()) &&
+                        !string.IsNullOrEmpty(Password.Text.Trim()) &&
+                        !string.IsNullOrEmpty(ConfirmPassword.Text.Trim()) &&
+                        (!Username.Text.Equals("Username") &&
+                         !Password.Text.Equals("Password") &&
+                         !ConfirmPassword.Text.Equals("Confirm Password")))
+                    {
+                        if (Password.Text.Equals(ConfirmPassword.Text))
+                        {
+                            mongoCollection.Insert(user);
+                            MessageBox.Show("สมัครเสร็จสิ้น");
+                        }
+                        else
+                        {
+                            MessageBox.Show("กรุณาใส่ Password ให้ตรงกัน");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex);
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Username.Text = "";
+            Password.Text = "";
+            ConfirmPassword.Text = "";
+        }
+
+        private void Username_Click(object sender, EventArgs e)
+        {
+            Username.Text = "";
+        }
+
+        private void Password_Click(object sender, EventArgs e)
+        {
+            Password.Text = "";
+            Password.UseSystemPasswordChar = true;
+        }
+
+        private void ConfirmPassword_Click(object sender, EventArgs e)
+        {
+            ConfirmPassword.Text = "";
+            ConfirmPassword.UseSystemPasswordChar = true;
+        }
+    }
+    class User
+    {
+        public ObjectId _id { get; set; }
+
+        public string Username
+        {
+            get; set;
+        }
+        public string Password
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+        public string Avatar
+        {
+            get; set;
+        }
+        public int Win
+        {
+            get; set;
+        }
+        public int Draw
+        {
+            get; set;
+        }
+        public int Lose
+        {
+            get; set;
+        }
+    }
+}
